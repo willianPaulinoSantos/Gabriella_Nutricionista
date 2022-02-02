a6da5c834a8884f215b2a49009c5ab8853f7b659
WIllian Paulino <willianpaulinox@gmail.com>
2022-02-02 11:40:56 -0300

    Inserido formulário no HTML e add func que adiciona paciente na tabela. N 04

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
 var pacientes = document.querySelectorAll(".paciente");
-console.log(pacientes);
+/*console.log(pacientes);*/
 
 var pesoValido = true;
 var alturaValida = true;
@@ -11,21 +11,86 @@ for (var i = 0; i < pacientes.length; i++){
 	var tdAltura = paciente.querySelector(".info-altura");
 	var tdImc = paciente.querySelector(".info-imc");
 
-	console.log(tdPeso);
+	/*console.log(tdPeso);
 	console.log(tdAltura);
-	console.log(tdImc);
+	console.log(tdImc);*/
 
 	var peso = tdPeso.textContent;
 	var altura = tdAltura.textContent;
 
-	console.log(peso);
-	console.log(altura);
+	/*console.log(peso);
+	console.log(altura);*/
 		
 	pesoValido = validaPeso(peso);
 	alturaValida = validaAltura(altura);
 	calculaImc(pesoValido, alturaValida);
 
 }
+
+
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function () {
+
+	event.preventDefault();
+	var form = document.querySelector("#form-adiciona");
+	var nome = form.nome.value;
+	var peso = form.peso.value;
+	var altura = form.altura.value;
+	var gordura = form.gordura.value;
+
+	/*console.log(form);
+	console.log(nome);
+	console.log(peso);
+	console.log(altura);
+	console.log(gordura);*/
+
+	var trPaciente = document.createElement("tr");
+	trPaciente.classList.add("paciente");
+
+	var tdNome = document.createElement("td");
+	tdNome.classList.add("info-nome");
+
+	var tdPeso = document.createElement("td");
+	tdPeso.classList.add("info-peso");
+
+	var tdAltura = document.createElement("td");
+	tdAltura.classList.add("info-altura");
+
+	var tdGordura = document.createElement("td");
+	tdGordura.classList.add("info-gordura");
+
+	var tdImc = document.createElement("td");
+	tdImc.classList.add("info-imc");
+
+	trPaciente.appendChild(tdNome);
+	trPaciente.appendChild(tdPeso);
+	trPaciente.appendChild(tdAltura);
+	trPaciente.appendChild(tdGordura);
+	trPaciente.appendChild(tdImc);
+
+	tdNome.textContent = nome;
+	tdPeso.textContent = peso;
+	tdAltura.textContent = altura;
+	tdGordura.textContent = gordura;
+	tdImc.textContent = (peso / (altura * altura)).toFixed(2);
+
+	var tabela = document.querySelector("#tabela-pacientes");
+	tabela.appendChild(trPaciente);
+
+	/*console.log(trPaciente);
+	console.log(tdNome);*/
+});
+
+
+
+
+
+
+
+
+
+
 
 function calculaImc(){
 
