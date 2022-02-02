a3111735dd1e2c4aa260219605863ec53787fd4c
WIllian Paulino <willianpaulinox@gmail.com>
2022-02-02 09:47:31 -0300

    Finalizada a funcionalidade de calcular o IMC de todos. Nível 03 - Arrays, Loops e estilos.

--- js/principal.js
+++ js/principal.js
@@ -1,37 +1,73 @@
-var paciente = document.querySelector("#primeiro-paciente");
-
-var tdPeso = paciente.querySelector(".info-peso");
-var tdAltura = paciente.querySelector(".info-altura");
-var tdImc = paciente.querySelector(".info-imc");
-
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+var pacientes = document.querySelectorAll(".paciente");
+console.log(pacientes);
 
 var pesoValido = true;
 var alturaValida = true;
 
 
-if (peso <= 0 || peso >= 595){
+for (var i = 0; i < pacientes.length; i++){
+
+	var paciente = pacientes[i];
+	var tdPeso = paciente.querySelector(".info-peso");
+	var tdAltura = paciente.querySelector(".info-altura");
+	var tdImc = paciente.querySelector(".info-imc");
+
+	/*console.log(tdPeso);
+	console.log(tdAltura);
+	console.log(tdImc);*/
+
+	var peso = tdPeso.textContent;
+	var altura = tdAltura.textContent;
+
+	/*console.log(peso);
+	console.log(altura);*/
+	
+
+	pesoValido = validaPeso(peso);
+	alturaValida = validaAltura(altura);
+	calculaImc(pesoValido, alturaValida);
+
+}
+
+
+
+
+function calculaImc(){
+
+	if (pesoValido && alturaValida){
+		
+		var imc = peso / (altura * altura);
+		tdImc.textContent = imc.toFixed(2);
+	}
+
+}
+
+
+function validaPeso(peso){
+
+	if (peso <= 0 || peso >= 595){
 
 	console.log("Peso inválido");
 	tdImc.textContent = "Peso inválido";
 	pesoValido = false;
-}
+	}
 
-if (altura <= 0 || altura > 2.50){
-
-	console.log("altura inválida");
-	tdImc.textContent = "Altura inválida";
-	alturaValida = false;
-}
-
-if (pesoValido && alturaValida){
-	
-	var imc = peso / (altura * altura);
-	tdImc.textContent = imc;
+	return pesoValido;
 }
 
 
+
+function validaAltura(altura){
+
+	if (altura <= 0 || altura > 2.50){
+
+		console.log("altura inválida");
+		tdImc.textContent = "Altura inválida";
+		alturaValida = false;
+	}
+
+	return alturaValida;
+}
 
 
 
