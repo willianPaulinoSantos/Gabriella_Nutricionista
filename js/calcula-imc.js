b64e6fba90cf501c43559b3cae10864740b86abf
WIllian Paulino <willianpaulinox@gmail.com>
2022-03-16 11:19:42 -0300

    Updating client's name.

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "Gabriella Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
